6afa19b004b7a4411e5ea04377733bc4a0c2215e
zack6yuan <zack6yuan@gmail.com>
2025-10-16 18:55:37 -0500

    starting my portfolio.

---
+++ Developer-Portfolio/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+export default function NavBar() {
+  return (
+    <div className="nav-bar">
+      <a href="/">
+        <p>Home</p>
+      </a>
+      <a href="">
+        <p>Projects</p>
+      </a>
+      <a href="">
+        <p>Skills</p>
+      </a>
+      <a href="">
+        <p>Contact</p>
+      </a>
+    </div>
+  )
+}
